a988e57649ee35aee8a6f820fa9e3882350a64a2
Alin Jerpelea <alin.jerpelea@sony.com>
2022-06-17 15:02:40 +0200

    Initial device configuration for Xperia 10 IV (PDX225)
    Basic configuration for Xperia 10 IV (PDX225) based on Sony Murray board
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ Android.mk
@@ -0,0 +1,7 @@
+ifeq (murray,$(PRODUCT_PLATFORM))
+
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-subdir-makefiles)
+
+endif
---
+++ rootdir/Android.mk
@@ -0,0 +1,65 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := vendor/etc/fstab.murray
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ramdisk-fstab.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := vendor/etc/fstab.murray
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := fstab.$(TARGET_DEVICE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.murray
+LOCAL_SRC_FILES := vendor/etc/init/init.murray.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.murray.pwr
+LOCAL_SRC_FILES := vendor/etc/init/init.murray.pwr.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ueventd
+LOCAL_SRC_FILES := vendor/ueventd.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+ifeq ($(USE_SENSOR_MULTI_HAL),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := hals.conf
+LOCAL_SRC_FILES := vendor/etc/sensors/hals.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/sensors
+include $(BUILD_PREBUILT)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sns_reg_config
+LOCAL_SRC_FILES := vendor/etc/sensors/sns_reg_config
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX :=
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/sensors
+include $(BUILD_PREBUILT)
